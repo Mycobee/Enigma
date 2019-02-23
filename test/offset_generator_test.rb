084a3024923749abc6622c141bb876ab1b0f4f3f
Rob Stringer <stringer525@gmail.com>
2019-02-23 14:59:57 -0700

    Instantiated offset generator class and corresponding test file

---
+++ test/offset_generator_test.rb
@@ -0,0 +1,11 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'pry'
+require './lib/key_generator'
+require './lib/date_generator'
+require './lib/offset_generator'
+
+class OffsetGeneratorTest < Minitest::Test
+
+
+end
