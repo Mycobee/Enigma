fce1257ca6ad025b1a047729db28396874d0617d
Rob Stringer <stringer525@gmail.com>
2019-02-24 15:29:16 -0700

    Deleted unnecessary function and test.

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -3,13 +3,17 @@ class Enigma
   attr_reader :message,
               :total_offset,
               :encryption_chars,
-              :cypher_text
+              :cypher_text,
+              :key,
+              :date
 
   def initialize(message)
     @message = message
     @total_offset = total_offset
     @encryption_chars = encryption_chars
     @cypher_text = cypher_text
+    @key = key
+    @date = date
   end
 
   def generate_offset
@@ -17,6 +21,8 @@ class Enigma
       offset.generate_key_offset
       offset.generate_date_offset
       offset.generate_total_offset
+      @key = offset.key
+      @date = offset.date
       @total_offset = offset.total_offset
   end
 
@@ -25,26 +31,9 @@ class Enigma
     offset.generate_key_offset
     offset.generate_date_offset
     offset.generate_total_offset
+    @key = offset.key
+    @date = offset.date
     @total_offset = offset.total_offset
-  end
-
-  def encrypt_character(char, shift)
-    char_set =
-    ["a", "b", "c", "d", "e", "f", "g", "h",
-      "i", "j", "k", "l", "m", "n", "o", "p",
-      "q", "r", "s", "t", "u", "v", "w", "x",
-       "y", "z", " "]
-    encrypted = []
-
-     char_set.each do |letter|
-         if letter == char.downcase
-           index_value = char_set.index(letter)
-           char_set.rotate!(shift)
-           shifted_character = char_set[index_value]
-           encrypted << shifted_character
-         end
-     end
-     encrypted[0]
   end
 
   def generate_encryption_chars
