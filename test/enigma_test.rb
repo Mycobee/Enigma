2f9f9f92835ea630995b80a906f58c08018c9008
Rob Stringer <stringer525@gmail.com>
2019-02-23 17:28:04 -0700

    Instantiated enigma class and test

---
+++ test/enigma_test.rb
@@ -0,0 +1,16 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'pry'
+require './lib/key_generator'
+require './lib/date_generator'
+require './lib/offset_generator'
+require './lib/enigma'
+
+class EnigmaTest < Minitest::Test
+
+  def test_it_exists
+    enigma = Enigma.new
+    assert_instance_of Enigma, enigma
+  end
+
+end
